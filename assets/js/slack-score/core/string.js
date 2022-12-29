a9016e4b2a9889be36fee807bcc2048d3ba98b65
Kevin <34973659+keketiger@users.noreply.github.com>
2022-12-29 05:20:59 +0100

    Réorganisation global

---
+++ assets/js/slack-score/core/string.js
@@ -0,0 +1,3 @@
+export const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1)
+
+export const slug = (string) => string.toLowerCase().replaceAll(' ', '-').replaceAll('\'', '')
