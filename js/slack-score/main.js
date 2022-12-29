055f98c48b03198a359b8c22c7673da26d63e5b5
Kevin <34973659+keketiger@users.noreply.github.com>
2022-12-29 05:18:43 +0100

    Delete js directory

--- js/slack-score/main.js
+++
@@ -1,5 +0,0 @@
-import { getToken } from './core/api.js'
-import { buildGuild } from './fetchers/guild.js'
-import { renderGuild } from './renderers/guild.js'
-
-getToken(buildGuild).then(renderGuild)
