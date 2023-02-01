fe320eb947bd43fb5f1a157205ff17b8246a0c3b
Debl0xMain <deblox160999@gmail.com>
2023-02-01 01:43:01 +0100

    add icon

--- assets/js/slack-score/renderers/player.js
+++ assets/js/slack-score/renderers/player.js
@@ -67,21 +67,18 @@ const printEnchantments = (enchantments) =>
         const unslugSlot = slot.toLowerCase().replace('_', '')
         var replacei = enchantments[slot].toLowerCase().replace(/[\s]+$/, '').split(/[\s]/).pop()
         const enchantClass = enchantments[slot] === 'No enchant' ? 'slack-enchant' : 'no-slack-enchant'
-        var test1 = enchantments[slot].toLowerCase().replace(replacei, '')
+        var replacetext = enchantments[slot].toLowerCase().replace(replacei, '')
 
 
         var replaceimg = enchantments[slot].toLowerCase().replace(/[\s]+$/, '').split(/[\s]/).pop()
         var rimg = replaceimg.replace('|a:professions-icon-quality-tier', '')
         var rimgi = rimg.replace('-small:20:20|a', '')
         var rimgir = rimgi.replace('enchant', '')
-       // <script>
-       // document.getElementById(enchantClass).replace
-       // </script>
 
         return `
             <div>
                 <span class="modal-subsection-title" id="modif">${LANG.ENCHANTABLE_SLOTS[unslugSlot]}</span>
-                <span class="${enchantClass}">${test1} Qualité ${rimgir}</span>
+                <span class="${enchantClass}" id="enchantsize">${replacetext}<img src="./assets/images/GaleryImg/qualite${rimgir}.png" alt=""></span>
             </div>
         `;
     }
